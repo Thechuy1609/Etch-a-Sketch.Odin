46eabfb10e8d6df430aabd8e4f2d99bcac6241c5
Thechuy1609 <blitzdashgd@gmail.com>
2024-09-11 16:24:52 -0500

    done with the user choice and made it to create grid squares by choice

--- script.js
+++ script.js
@@ -47,18 +47,19 @@ function getSquaresNumber(){
          alert("Please enter a valid number");
      }
 
-    
-    });
-      };
+})}
 
 
 function getRandomColor(){
+    const buttonR = document.querySelector('#button3')
+    buttonR.addEventListener("click" , () => {
         let letters = '0123456789ABCDEF';
         let color = '#';
         for (var i = 0; i < 6; i++) {
           color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
+    })
       }
 
 
